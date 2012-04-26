cfb68d6e2074e9adf633c1cccc540bb12a455d24
Mike Minutillo <michael.minutillo@gmail.com>
2012-04-26 21:57:11 +0800

    Basic Infrastructure

---
+++ Forklift/Commands.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forklift
+{
+    public interface ICommand
+    {
+        void Run(Args args);
+    }
+
+    public abstract class CommandBase : ICommand
+    {
+        public void Run(Args args)
+        {
+            var planFile = args.Option("plan", "forklift.plan");
+            var extractFile = args.Option("extract", "extract.xml");
+
+            Console.WriteLine("Plan:    {0}", planFile);
+            Console.WriteLine("Extract: {0}", extractFile);
+
+            RunCore(args);
+        }
+
+        protected abstract void RunCore(Args args);
+    }
+
+    public class PushCommand : CommandBase
+    {
+
+        protected override void RunCore(Args args)
+        {
+            Console.WriteLine("Pushing");
+        }
+    }
+
+    public class PullCommand : CommandBase
+    {
+        protected override void RunCore(Args args)
+        {
+            Console.WriteLine("Pulling");
+        }
+    }
+
+    class Commands
+    {
+        private static Lazy<ILookup<string, Type>> _commands = new Lazy<ILookup<string, Type>>(() =>
+            typeof(Commands).Assembly.GetTypes().Where(x => x.GetInterfaces().Contains(typeof(ICommand)))
+            .ToLookup(x => x.Name.Replace("Command", "").ToLower()));
+
+        public static ILookup<string, Type> All
+        {
+            get { return _commands.Value; }
+        }
+
+        public static ICommand Find(string name)
+        {
+            var type = All[name].FirstOrDefault();
+            if (type == null)
+                return null;
+
+            return (ICommand)Activator.CreateInstance(type);
+        }
+    }
+}
---
+++ Forklift/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Forklift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var parsedArgs = Args.Parse(args);
+
+                var commandName = parsedArgs.Arguments.FirstOrDefault();
+
+                if (commandName == null)
+                {
+                    Console.Error.WriteLine("You must supply a command name");
+                    ShowUsageAndExit(-1);
+                }
+
+                var command = Commands.Find(commandName);
+
+                if (command == null)
+                {
+                    Console.Error.WriteLine("No command with that name could be found");
+                    ShowUsageAndExit(-2);
+                }
+
+                command.Run(parsedArgs);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Something went wrong: {0}", ex);
+                ShowUsageAndExit(-3);
+            }
+
+        }
+
+        private static void ShowUsageAndExit(int p)
+        {
+            Console.WriteLine("USAGE: forklift.exe <command> <command args> [<options>]");
+            Environment.Exit(p);
+        }
+    }
+
+    public class Args
+    {
+        private readonly IDictionary<string, string> _options = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+        private readonly IList<string> _arguments = new List<string>();
+
+        public string[] Arguments
+        {
+            get
+            {
+                return _arguments.ToArray();
+            }
+        }
+
+        public string Option(string name, string @default = null)
+        {
+            if (_options.ContainsKey(name))
+                return _options[name];
+            return @default;
+        }
+
+        private Args()
+        {
+        }
+
+        public static Args Parse(string[] args)
+        {
+            var parsedArgs = new Args();
+
+            var counter = 0;
+            while (counter < args.Length)
+            {
+                var arg = args[counter];
+                if (arg.StartsWith("-"))
+                {
+                    var name = arg.Substring(1);
+                    counter += 1;
+                    if (counter >= args.Length)
+                        throw new Exception("No value provided for option " + name);
+                    var value = args[counter];
+                    parsedArgs._options.Add(name, value);
+                }
+                else
+                {
+                    parsedArgs._arguments.Add(arg);
+                }
+                counter += 1;
+            }
+
+
+            return parsedArgs;
+        }
+    }
+}
